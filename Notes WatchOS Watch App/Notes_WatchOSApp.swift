7a55d0929a37f61838ad09e833f766e4018b0abe
Vl-VSV <vvs_2015@icloud.com>
2022-11-06 20:15:58 +0300

    Initial Commit

---
+++ Notes WatchOS Watch App/Notes_WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_WatchOSApp.swift
+//  Notes WatchOS Watch App
+//
+//  Created by Vlad V on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Notes_WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
